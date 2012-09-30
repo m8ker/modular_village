9fe0124eaaf509bdd76cd9888ca90ab27e2769e8
Justin Haight <justin@calendar.fm>
2012-09-30 16:34:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModularVillage::Application.config.secret_token = 'a5c577408aea72278cb6c2f67942e36a5d934dc84de5ab55372bd30f2f13ee94d5dddff26ede8a3d253af31a24597cc83915546593cfd121b47cc179072c11c0'
