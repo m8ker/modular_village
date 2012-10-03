50c4580f5a2dada86daac52246b6acf8028bc896
Justin Haight <justin@calendar.fm>
2012-10-03 02:45:46 -0700

    fixed error in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ ModularVillage::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => 'modularvillage.org }
+  config.action_mailer.default_url_options = { :host => 'modularvillage.org' }
 end
