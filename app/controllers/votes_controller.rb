4a5b1f941218a797c09223ad3f8f9cc053244535
Justin Haight <justin@calendar.fm>
2012-10-02 20:07:19 -0700

    basic commenting operational

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,17 @@
 class VotesController < ApplicationController
-  
-  before_filter :authenticate_user!
-  
-  def create
-    @vote = current_user.votes.create(params[:up])
-    redirect_to :back
-  end
-  
+
+	before_filter :authenticate_user!
+	# def new
+	# 	@vote = Vote.new
+	# end
+
+	def create
+		@vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first
+		@vote ||= current_user.votes.create(params[:vote])
+		@vote.update_attributes(:up => params[:vote][:up])
+		#puts "========================"
+		#puts @vote.errors.inspect
+		redirect_to :back
+	end
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,7 @@
 class Vote < ActiveRecord::Base
-  attr_accessible :link_id, :up
+  attr_accessible :link_id, :up, :user_id
+  
+  validates :user_id, :uniqueness => { :scope => :link_id }
   
   belongs_to :user
   belongs_to :link
