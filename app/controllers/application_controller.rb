ed0b4a1719aa4655029d7235d9cb4768e91140de
Justin Haight <justin@calendar.fm>
2012-10-03 23:19:06 -0700

    bug fix - main_app.root_url in redirecto to fixes cancan incompatability with rails_admin on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     if is_cancan
       rescue_from CanCan::AccessDenied do |exception|
         flash[:error] = 'Access denied.'
-        redirect_to root_url
+        redirect_to main_app.root_url
       end
     end
 end
